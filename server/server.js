a0c4bc8b2e46e4d0496c587717011394adb214c4
Ross Landgraf <rlandgra@uncc.edu>
2022-12-09 21:18:12 +0000

    Added comments to server.js to make it more clear

--- server/server.js
+++ server/server.js
@@ -9,14 +9,12 @@ const fs = require("fs");
 const cors = require('cors')
 app.use(cors())
 
-
+//need this if using curl commands instead of utilizing the html form
 //var jsonParser = bodyParser.json()
-
+//app.use(express.json());
 
 
 app.use(bodyParser.urlencoded({ extended: true }));
-
-//app.use(express.json());
 
 //Imports the API Key and Endpoint from the config folder so that they are not exposed to the internet:
 const { computerVisionAPIKey, computerVisionEndpoint } = require("./config");
@@ -24,16 +22,23 @@ const computerVisionClient = new ComputerVisionClient(
   new ApiKeyCredentials({ inHeader: { 'Ocp-Apim-Subscription-Key': computerVisionAPIKey } }), computerVisionEndpoint);
 
 // Used information from https://learn.microsoft.com/en-us/azure/cognitive-services/computer-vision/quickstarts-sdk/image-analysis-client-library?tabs=visual-studio%2C3-2&pivots=programming-language-javascript
+
 //Implements Text Extraction API feature:
 app.post("/textextraction", (req, res) => {
+  //prints information to console to visualize it running. To see these you must run with node.
   console.log("running text extraction...");
   console.log(req.body);
+  //Verifies that a url was submitted, if not it returns an error:
   if (!req.body?.image_url) return res.status(400).send({ data: "No url given" });
-
+  //Establishes the computer vision client to recognize printed text:
   computerVisionClient.recognizePrintedText(true, req.body.image_url).then((data) => {
+    //Prints to console data that is extracted from the text after it is sent through a function that loops through the object in order to exract the text and put it all together
     console.log(getText(data));
+    //Logs submitted url and the text extracted to a log file on the server. This is used to verify everything is working correctly and review if it was not you testing:
     logText("URL: " + req.body.image_url + " Text Extracted: " + getText(data))
+    //Sends the data and the text to the html/website:
     res.send({ data: getText(data) })
+    //Catches any errors and sends those errors to both the server side log and to the html/website:
   }).catch((err) => {
     console.error(err);
     logText("/textextraction Error: " + err.message + " URL: " + req.body.image_url);
